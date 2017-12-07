e78d25fd6ad49e67315968ec6419ae11f76e6aa4
neliza <nicolasaelf64@gmail.com>
2017-12-07 14:39:43 -0700

    almost there

--- reactmatch/src/App.js
+++ reactmatch/src/App.js
@@ -13,7 +13,9 @@ class App extends Component {
     // toggleClick: false
     artists:artists, 
     score: 0, 
-    topScore: 0
+    topScore: 0, 
+    clickedName: "", 
+    youLose: ""
   } 
 
   //Handle image click event  
@@ -22,35 +24,65 @@ class App extends Component {
     this.state.artists.forEach((artist) => {
       //I want to change click from false to true
       if(artist.id === id) {
+        this.setState({clickedName: artist.name})
         if(artist.clicked){
           console.log(`${artist.name} has ALREADY been clicked`)
           //This is where you reset the game, because player clicked on artist card 
             //That was previously clicked
-            this.resetGame();
-        }
+            this.setState({youLose: "You Lose! Game Over!"}, () => this.resetGame())          
+            // this.resetGame();
+          }
         else {
           artist.clicked = true;
           console.log(`${artist.name} click status is ${artist.clicked}`)
           //You want to update the score here, since this where where the artist card
             //is initially clicked
             this.increaseScore();
-            this.updateTopScoreState();
+            // shuffle artists array
+            setTimeout(() => this.shuffleArtistArray(this.state.artists), 200);
         }
       }                     
     })
   }
+
+  //Shuffle function graciously borrowed from: 
+  //https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+  shuffleArtistArray = (array) => {    
+      var currentIndex = array.length, temporaryValue, randomIndex;
+    
+      // While there remain elements to shuffle...
+      while (0 !== currentIndex) {
+    
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+    
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+        }
+        
+        this.setState({artists:array}, 
+          () => {
+            console.log("Rearranging array now...")
+        })         
+    }      
   
   //Function to increase the score when the artist card is initially
+  //
   increaseScore = () => {
-    this.setState({score: this.state.score + 1}) 
+    this.setState({score: this.state.score + 1}, () => this.updateTopScoreState())     
   }
 
   //Function to update the top score
   updateTopScoreState = (event) => {
     //compare score and topScore
+    console.log(`${this.state.score} and ${this.state.topScore}`)
     if(this.state.score > this.state.topScore){
       this.setState({topScore : this.state.score}, 
         () => {
+          console.log(`HERE IS YOUR TOP SCORE!`)
           console.log(this.state.topScore)
         }
     )}
@@ -61,7 +93,12 @@ class App extends Component {
     console.log('You Lose! Resetting the game!')
     //When resetting the game, I am only resetting the score and not the topScore
       //So that when the game "resets", the top score remains
-    this.setState({score: 0});
+    this.setState({
+      score: 0, 
+      clickedName: "",
+      youLose:""
+    });
+
     this.state.artists.forEach((artist) => {
       artist.clicked = false;
       //Check to make sure the click status is being reset to false      
@@ -76,7 +113,7 @@ class App extends Component {
     // console.log(this.state)
     return (
       <Wrapper>        
-        <Header score={this.state.score} topScore={this.state.topScore}/>
+        <Header score={this.state.score} topScore={this.state.topScore} clickedName={this.state.clickedName} youLose={this.state.youLose}/>
           <div>
             {this.state.artists.map(artist => {
               return <ArtistCard {...artist}
--- reactmatch/src/components/Header/Header.js
+++ reactmatch/src/components/Header/Header.js
@@ -5,7 +5,9 @@ const Header = props => (
     <header className="header">
         <h3>Artist Memory Game</h3>
             <h4>Basically, don't click the same image twice...</h4>
-            <h4>Score: {props.score} || Top Score: {props.topScore}</h4>           
+            <h4>Score: {props.score} || Top Score: {props.topScore}</h4>
+            <h4>You Clicked: {props.clickedName}</h4>  
+            <h5> {props.youLose}</h5>
     </header>
 )
 
